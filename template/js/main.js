a13ae0418f3516cb6b57b012270ab183dd408fd5
Zuri Walters <walterszuri02@gmail.com>
2021-03-21 09:46:41 -0400

    First commit

---
+++ template/js/main.js
@@ -0,0 +1,62 @@
+
+document.querySelector('#aries').addEventListener('click', aries)
+document.querySelector('#taurus').addEventListener('click', taur)
+document.querySelector('#geminai').addEventListener('click', gem)
+document.querySelector('#cancer').addEventListener('click', cancer)
+document.querySelector('#leo').addEventListener('click', leo)
+document.querySelector('#virgo').addEventListener('click', vir)
+document.querySelector('#libra').addEventListener('click', lib)
+document.querySelector('#scorpio').addEventListener('click', sco)
+document.querySelector('#sag').addEventListener('click', sag)
+document.querySelector('#cap').addEventListener('click', cap)
+document.querySelector('#aqua').addEventListener('click', aqua)
+document.querySelector('#pisces').addEventListener('click', pisces)
+
+
+function aries(){
+  document.querySelector('#dis').innerText = "February 2021 - February arrives with a major focus on your finances, Aries. This is because your ruler Mars powers up this sector again this month. It could increase your expenses during this time but also give you the drive to make so much more.This would be an excellent time to look for a better paying job or, if you freelance, hunt down more clients."
+}
+
+function taur(){
+  document.querySelector('#dis').innerText ="February 2021 - You are nearly unstoppable in February, Taurus! That’s because you’re energized by the tremendous power of Mars through the entire month. You’ll be fired up to tackle your goals and able to make a great deal of progress on any of your personal or professional plans. Take the initiative. Be a leader. Don’t sit in the shadows."
+}
+
+function gem(){
+  document.querySelector('#dis').innerText ="February 2021 - February could feel especially slow for you, Gemini. This is because your ruler Mercury is fast asleep in a retrograde cycle until February 20. Even after he awakens, though, he will be groggily catching up to speed through the remaining days."
+}
+
+function cancer(){
+  document.querySelector('#dis').innerText ="February 2021 - As mighty Mars ignites your solar eleventh house of community, you’ll feel your social life highly active this month. This will likely bring exciting news from friends as well as possibilities for events and gatherings. If you’re able to make some time for fun and adventure, go for it. You’ll be glad you did!"
+}
+
+function leo(){
+  document.querySelector('#dis').innerText ="February 2021 - Your career will be shooting off fireworks in February, Leo! Mars is skyrocketing through your solar tenth house of achievement, which can make this a tremendously exciting time for you to pursue professional goals. Set important meetings with business associates, pitch your plans, or search for a higher-paying job. Promotions, accolades, and public recognition may manifest now from previously completed work."
+}
+
+function vir(){
+  document.querySelector('#dis').innerText ="February 2021 - With so much activity in your solar sixth house of productivity, it’s clear that you have many irons in the fire! This bodes well for Virgos who are looking for work or interested in taking on more projects with their current employer."
+}
+
+function lib(){
+  document.querySelector('#dis').innerText ="February 2021 - The stars sparkle divinely for you in February, Libra. Get ready to open your heart wide as the planets rain showers of love upon you!"
+}
+
+function sco(){
+  document.querySelector('#dis').innerText ="February 2021 - A slower pace of life is guaranteed in February, Scorpio. This is because Mercury, planet of communication and clarity, is spinning backward until the twentieth. This could have you reassessing or reconsidering some plans regarding a home, real estate, or family matter. Despite some delays or uncertainty, a great deal of planetary energy can instead bring opportunities for growth and happiness to these same areas."
+}
+
+function sag(){
+  document.querySelector('#dis').innerText ="February 2021 - Your mind could be especially active this month with so much planetary activity lighting up this area of your life, Sagittarius. Venus soars into your solar third house of communication on February 1 and remains here until February 25. This means that you can find the perfect words to charm anyone you want to impress."
+}
+
+function cap(){
+  document.querySelector('#dis').innerText ="February 2021 - February guarantees that your mind is highly focused on your income and prosperity, Capricorn. While you always like to build your life like a business, you’re especially favored to increase your abundance this month and throughout 2021."
+}
+
+function aqua(){
+  document.querySelector('#dis').innerText ="February 2021 - You are the cosmic favorite at this time, Aquarius! With so much planetary power in your zodiac sign, you have a rare opportunity to direct all events in your favor. The sun, Mercury, Jupiter, and Saturn have already been spinning within your grasp, highlighting your personal goals and plans. Venus joins you on February 1 and stays until February 25. With the goddess of pleasure within you, now is a great time to update your look or embrace romance."
+}
+
+function pisces(){
+  document.querySelector('#dis').innerText ="February 2021 - With so much planetary activity in your solar twelfth house, it’s clear that you have a great deal of activity behind the scenes, Pisces. This could be increasing your intuitive senses or the intensity of your dreams. Pay attention to the messages from the universe now because you could have your finger on the pulse of some meaningful breakthroughs. This same sector of the sky also rules working on projects in private or developing things that will take some time to get right, so by all means let your creative juices flow and brainstorm!"
+}
